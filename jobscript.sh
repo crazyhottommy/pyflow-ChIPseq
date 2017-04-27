2cb7943dfb2d89fbf7f6f26c9dc36414d5ee4db0
Tommy Tang <tangming2005@gmail.com>
2017-04-27 14:10:05 -0500

    keep the default snakemake jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -7,4 +7,4 @@
 # if the job succeeds, snakemake 
 # touches jobfinished, thus if it exists cat succeeds. if cat fails, the error code indicates job failure
 # see https://groups.google.com/forum/#!searchin/snakemake/immediate$20submit%7Csort:relevance/snakemake/1QelazgzilY/mz0KfAzJAgAJ
-cat $1
+#cat $1
