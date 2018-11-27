901d8b09c7486765bc2ce7ee81c86c7e57e48473
Ming Tang <tangming2005@gmail.com>
2018-11-27 15:33:02 -0500

    submit to cluster wrapper

--- pyflow-ChIPseq.sh
+++ pyflow-ChIPseq.sh
@@ -3,9 +3,26 @@
 
 # invoke Snakemake in cluster mode with custom wrapper scripts
 
-snakemake --rerun-incomplete -j 1000 -k \
-		  --jobscript ./jobscript.sh \
-		  --latency-wait 120 \
-		  --cluster-config cluster.json --cluster './sbatch_cluster.py' \
-		  "$@"
+#snakemake --rerun-incomplete -j 1000 -k \
+#		  --jobscript ./jobscript.sh \
+#		  --latency-wait 120 \
+#		  --cluster-config cluster.json --cluster './sbatch_cluster.py' \
+#		  "$@"
+
+#! /bin/bash
+
+## make a folder for bsub_log if not exist
+folder="sbatch_log"
+
+
+if [ ! -d "$folder" ]; then
+    mkdir -p "$folder"
+fi
+
+snakemake --jobs 99999 \
+    --latency-wait 240 \
+    --cluster-config cluster.json \
+    --cluster "sbatch --partition {cluster.p} --mem {cluster.mem} -N {cluster.N} -n {cluster.n} --time {cluster.time} --job-name {cluster.name} --output {cluster.output} --error {cluster.error}" \
+    "$@"
+	 
 
